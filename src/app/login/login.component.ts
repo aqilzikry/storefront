cb032db0910347e4c8e77025ca97ccbbecc28004
aqilzikry <aqilzikry97@gmail.com>
2023-10-29 04:16:32 +0800

    Bind login form to service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formGroup!: FormGroup;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login() {
+    if (this.formGroup.valid) {
+      this.authService.login(this.formGroup.value).subscribe((result) => {
+        if (result.status) {
+          console.dir(result.data.token);
+        } else {
+          console.log(result.message);
+        }
+      });
+    }
+  }
 }
