02422bb3c94ed2ac5c20eae1cf031ad8f3d8e2dc
aqilzikry <aqilzikry97@gmail.com>
2023-10-30 02:00:29 +0800

    Show auth status in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   loggedIn = false;
+  currentUser!: User;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    console.log('Subscribing.');
+    this.authService.currentUser.subscribe((user) => {
+      console.dir(user);
+      this.currentUser = user;
+      this.loggedIn = !!user;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
