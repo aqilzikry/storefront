1797da8c0ead0d0bf3a6e7324f9583c2912ff4e3
aqilzikry <aqilzikry97@gmail.com>
2023-10-30 02:00:12 +0800

    Persist login state

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,19 +1,52 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { User } from './interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(null);
+    this.currentUser = this.currentUserSubject.asObservable();
+
+    // Initialize the user from local storage on service creation
+    const storedUser = localStorage.getItem('currentUser');
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      this.currentUserSubject.next(user);
+    }
+  }
 
   register(data: any): Observable<any> {
-    return this.http.post('https://localhost:7027/api/authentication/register', data);
+    return this.http.post(
+      'https://localhost:7027/api/authentication/register',
+      data
+    );
   }
 
   login(data: any): Observable<any> {
-    return this.http.post('https://localhost:7027/api/authentication/login', data);
+    return this.http
+      .post('https://localhost:7027/api/authentication/login', data)
+      .pipe(
+        map((response: any) => {
+          console.log('Posting');
+          if (response.status) {
+            const user = response.data.user;
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+          }
+          return response;
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
   }
 }
