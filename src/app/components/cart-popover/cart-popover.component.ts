86dc3932d50c2901bd78556c13f3c5ddc01cbdd2
aqilzikry <aqilzikry97@gmail.com>
2023-10-31 05:50:51 +0800

    fix: Close cart popover on click outside

--- src/app/components/cart-popover/cart-popover.component.ts
+++ src/app/components/cart-popover/cart-popover.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cart-popover',
@@ -7,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class CartPopoverComponent {
   test = [1, 2];
+  isDropdownOpen = false;
+
+  @ViewChild('myDetailsElement') detailsElement!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (!this.detailsElement.nativeElement.contains(event.target)) {
+      this.renderer.removeAttribute(this.detailsElement.nativeElement, 'open');
+    }
+  }
 }
