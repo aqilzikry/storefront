039daa4d3a530cad1fafabca2f1bade95a05820c
Aqilzikry Arman <62475779+aqilzikry@users.noreply.github.com>
2023-11-04 21:16:01 +0800

    Feature/cart (#9)
    * Display no items message if cart empty
    
    * chore: Add SSL to dev server and formatting
    
    * Add cart service
    
    * Fetch cart after login
    
    * Clear user token and cart after logout.
    
    * Update log message for user subscription
    
    * Cart states after login and logout
    
    * Remove unused imports
    
    * Initiate cart data from backend

---
+++ src/app/cart.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  private apiUrl = 'https://localhost:7027/api/Cart';
+
+  private cartItemsSubject = new BehaviorSubject<any>(null);
+  cartItems$: Observable<any> = this.cartItemsSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      const user = JSON.parse(storedCart);
+      this.cartItemsSubject.next(user);
+    }
+  }
+
+  getCartItems(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get(this.apiUrl, { headers }).pipe(
+      tap((data) => {
+        this.cartItemsSubject.next(data);
+      })
+    );
+  }
+
+  clearCartItems(): void {
+    this.cartItemsSubject.next(null);
+    localStorage.removeItem('cart');
+  }
+}
