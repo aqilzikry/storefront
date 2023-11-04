a41c1a11f58e8797152c9f0dded5c1b8193dc143
aqilzikry <aqilzikry97@gmail.com>
2023-11-04 21:01:16 +0800

    Cart states after login and logout

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,13 +9,32 @@ import { AuthService } from './auth.service';
 export class CartService {
   private apiUrl = 'https://localhost:7027/api/Cart';
 
-  constructor(private http: HttpClient) {}
+  private cartItemsSubject = new BehaviorSubject<any>(null);
+  cartItems$: Observable<any> = this.cartItemsSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      const user = JSON.parse(storedCart);
+      this.cartItemsSubject.next(user);
+    }
+  }
 
   getCartItems(): Observable<any> {
     const token = localStorage.getItem('token');
-    const headers = {
+    const headers = new HttpHeaders({
       Authorization: `Bearer ${token}`,
-    };
-    return this.http.get(this.apiUrl, { headers });
+    });
+
+    return this.http.get(this.apiUrl, { headers }).pipe(
+      tap((data) => {
+        this.cartItemsSubject.next(data);
+      })
+    );
+  }
+
+  clearCartItems(): void {
+    this.cartItemsSubject.next(null);
+    localStorage.removeItem('cart');
   }
 }
