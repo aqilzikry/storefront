474babe65bbaff5e059d4b03b7b0fe7f6487fdc9
aqilzikry <aqilzikry97@gmail.com>
2023-11-04 22:05:53 +0800

    Refactor cart data init in Cart Popover

--- src/app/components/cart-popover/cart-popover.component.ts
+++ src/app/components/cart-popover/cart-popover.component.ts
@@ -30,19 +30,12 @@ export class CartPopoverComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe((user) => {
-      if (user) {
-        console.log('Subscribing to cart...');
-        this.cartService.getCartItems().subscribe((data) => {
-          this.cart = data.data;
+    this.cartService.getCartItems().subscribe();
+    this.cartService.cartItems$.subscribe((data) => {
+      this.cart = data.data;
 
-          if (this.cart && this.cart.cartItems) {
-            this.itemCount = this.cart.cartItems.length;
-          }
-        });
-      } else {
-        this.cart = null;
-        this.itemCount = 0;
+      if (this.cart && this.cart.cartItems) {
+        this.itemCount = this.cart.cartItems.length;
       }
     });
   }
